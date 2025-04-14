9cf3b6ca5895d11535b87aac640d58d45ba3bafd
Madiyar <110019387+folk-print@users.noreply.github.com>
2025-04-14 18:13:26 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -49,6 +49,15 @@ export default function Home() {
             }
           `,
         }} />
+          <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-01ZJXKDZ9D"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-01ZJXKDZ9D');
+</script>
       </Head>
 
       <Layout>
