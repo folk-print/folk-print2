3df8e3f37fc850197657af3008343cc1ad82c135
Madiyar <110019387+folk-print@users.noreply.github.com>
2025-04-14 17:58:41 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,21 @@ import Iframe from "../components/iFrame";
 import Layout from "../components/Layout";
 import Partners from "../components/partners";
 import Products from "../components/products";
+
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Качественная корпоративная одежда с логотипом в Узбекистане | Folk Print</title>
         <meta name="Folk print" content="Качественная корпоративная одежда с логотипом" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
           rel="icon"
           type="image/x-icon"
           href="https://i.postimg.cc/kMsnVdJS/001.png"
-        ></link>
-        {/* Google Tag (gtag.js) */}
+        />
+
+        {/* Global site tag (gtag.js) */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17006444297"></script>
         <script dangerouslySetInnerHTML={{
           __html: `
@@ -27,15 +29,28 @@ export default function Home() {
             gtag('config', 'AW-17006444297');
           `,
         }} />
-  <!-- Event snippet for Просмотр страницы conversion page -->
-<script>
-  gtag('event', 'conversion', {
-      'send_to': 'AW-17006444297/8kVNCOCJprgaEIn-pq0_',
-      'value': 1.0,
-      'currency': 'USD'
-  });
-</script>
+
+        {/* Conversion Event Function */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            function gtag_report_conversion(url) {
+              var callback = function () {
+                if (typeof(url) !== 'undefined') {
+                  window.location = url;
+                }
+              };
+              gtag('event', 'conversion', {
+                'send_to': 'AW-17006444297/8kVNCOCJprgaEIn-pq0_',
+                'value': 1.0,
+                'currency': 'USD',
+                'event_callback': callback
+              });
+              return false;
+            }
+          `,
+        }} />
       </Head>
+
       <Layout>
         <About />
         <Products />
