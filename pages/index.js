1f66d1f701a7579c66b2b029f25cc3f346266a1e
Madiyar <110019387+folk-print@users.noreply.github.com>
2025-04-15 22:45:05 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,49 +5,19 @@ import Iframe from "../components/iFrame";
 import Layout from "../components/Layout";
 import Partners from "../components/partners";
 import Products from "../components/products";
-import Script from "next/script";
-
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Корпоративная одежда с логотипом в Узбекистане</title>
+        <title>Folk Print</title>
         <meta name="Folk print" content="Качественная корпоративная одежда с логотипом" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <link
           rel="icon"
           type="image/x-icon"
           href="https://i.postimg.cc/kMsnVdJS/001.png"
-        />
+        ></link>
       </Head>
-
-      {/* Google Tag Manager Global Site Script */}
-      <Script 
-        strategy="afterInteractive" 
-        src={https://www.googletagmanager.com/gtag/js?id=AW-17006444297}
-      />
-      
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: 
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            
-            gtag('config', 'AW-17006444297');
-            gtag('config', 'G-01ZJXKDZ9D');
-            
-            gtag('event', 'conversion', {
-              'send_to': 'AW-17006444297/8kVNCOCJprgaEIn-pq0_',
-              'value': 1.0,
-              'currency': 'USD'
-            });
-          ,
-        }}
-      />
-
       <Layout>
         <About />
         <Products />
