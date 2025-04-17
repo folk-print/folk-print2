99126fc36c8fc8fbbcb21249433f0c354ae576a1
Madiyar <110019387+folk-print@users.noreply.github.com>
2025-04-17 11:28:24 +0500

    Update ContactForm.js

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -40,7 +40,7 @@ export default function ContactForm({ onClose }) {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          chat_id: "895653052",
+          chat_id: "5061986357",
           text: message,
         }),
       });
